4dcfc4134474f3246df2ed9b57413878c8980788
Yurii Smirnov <ipzm191_syuo@student.ztu.edu.ua>
2021-10-23 12:30:46 +0300

    Calculator

---
+++ src/main/java/calc/Calculator.java
@@ -0,0 +1,19 @@
+package calc;
+
+public class Calculator {
+    public Double getSum(double a, double b) {
+        return a + b;
+    }
+
+    public Double getSubtraction(double a, double b) {
+        return a - b;
+    }
+
+    public Double getMultiple(double a, double b) {
+        return a * b;
+    }
+
+    public Double getDivision(double a, double b) {
+        return a / b;
+    }
+}
