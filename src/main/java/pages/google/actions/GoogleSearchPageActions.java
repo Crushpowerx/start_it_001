0464ee8aec1d3cd190d30ce06a1ce69d37ae5662
Crushpowerx <asovin.e.v@gmail.com>
2021-10-30 11:12:21 +0300

    upd

---
+++ src/main/java/pages/google/actions/GoogleSearchPageActions.java
@@ -0,0 +1,11 @@
+package pages.google.actions;
+
+import io.qameta.allure.Step;
+
+public class GoogleSearchPageActions {
+
+    @Step
+    public void fillInputSearch(String text) {
+
+    }
+}
---
+++ src/main/java/pages/google/locators/GoogleSearchPageLocators.java
@@ -0,0 +1,10 @@
+package pages.google.locators;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+
+public class GoogleSearchPageLocators {
+    public final SelenideElement inputSearch = $(byXpath("//input[@title='Поиск']"));
+}
