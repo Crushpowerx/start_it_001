4e45c641e8ed6b4dbbc933f8a098d9d7bbbc9b6a
Crushpowerx <asovin.e.v@gmail.com>
2021-11-06 10:53:05 +0200

    upd

--- src/test/java/ui/google/GoogleSearchTest.java
+++ src/test/java/ui/google/GoogleSearchTest.java
@@ -1,18 +1,23 @@
 package ui.google;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ui.AbstractBaseTest;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class GoogleSearchTest extends AbstractBaseTest {
-    private final String googleSearchUrl = "https://www.google.com/";
+    private final String googleSearchUrl = "https://username:password@www.google.com/";
     private final String textForGoogleSearch = "Юрий Смирнов бильярд видео";
     private final String textForGoogleSearchResult = "Кубок Киева. Финал. Павел Радионов - Юрий Смирнов";
 
+    @BeforeMethod
+    public void openUrl() {
+        open(googleSearchUrl);
+    }
+
     @Test
     public void checkGoogleSearchResults() {
-        open(googleSearchUrl);
         googleSearchPage.fillInputSearch(textForGoogleSearch);
         googleSearchPage.clickButtonSearch();
         googleSearchResultPage.countSearchResults();
