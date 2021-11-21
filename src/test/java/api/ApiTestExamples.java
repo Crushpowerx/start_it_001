f486e1444ea0d5c0290ebc4036e5b77aa09402a2
Crushpowerx <asovin.e.v@gmail.com>
2021-11-21 10:15:20 +0200

    WIP

--- src/test/java/api/ApiTestExamples.java
+++ src/test/java/api/ApiTestExamples.java
@@ -1,5 +1,8 @@
 package api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -22,5 +25,24 @@ public class ApiTestExamples {
                 .all();
     }
 
+    @Test
+    public void getJsonFromHtml() throws JsonProcessingException {
+        String url = "http://demo.guru99.com/V4/sinkministatement.php?CUSTOMER_ID=68195&PASSWORD=1234!&Account_No=1";
+        ObjectMapper objectMapper = new ObjectMapper();
+        String responseHtmlBody = given()
+                .when()
+                .get(url)
+                .htmlPath()
+                .get("html.body")
+                .toString();
+        JsonNode bodyJson = objectMapper.readTree(responseHtmlBody);
+        int sumOfAll = 0;
+        for (JsonNode s : bodyJson.get("result:").get("statements")) {
+            String a = s.get("AMOUNT").textValue();
+            System.out.println("The amount value fetched is " + a);
+            sumOfAll = sumOfAll + Integer.parseInt(a);
+        }
+        System.out.println("The total amount is " + sumOfAll);
+    }
 
 }
